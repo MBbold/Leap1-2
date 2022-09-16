fbe6d179dbdac745c6abe66d865bca77eed67d01
MBbold <103709596+MBbold@users.noreply.github.com>
2022-09-16 08:02:27 +0800

    createUserWithEmailAndPassword update

--- adminpage-app/src/firebase/firebaseConfig.js
+++ adminpage-app/src/firebase/firebaseConfig.js
@@ -1,13 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from "./firebaseKey";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendSignInLinkToEmail} from "firebase/auth";
 import { useEffect, useState } from "react";
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
 export default app
 export const getSignUp =(email, password )=>{
-    return createUserWithEmailAndPassword(auth, email, password)
+    sendSignInLinkToEmail(email,{
+        url:'http://localhost:3000/signup'
+        // console.log("sss")
+    })
+    return createUserWithEmailAndPassword(auth, email, password) 
 }
 export const getLogIn =(email, password )=>{
     console.log("Newtrew")
